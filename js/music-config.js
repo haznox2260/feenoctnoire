4d184d86c730bdee27ac88c35487293e05f20e06
haznox2260 <tinhha226@gmail.com>
2026-06-26 15:01:52 +0700

    Add files via upload

---
+++ js/music-config.js
@@ -0,0 +1,29 @@
+// Khai báo TRƯỚC music-player.js
+// Thêm bài: bỏ MP3 vào thư mục /music/ rồi thêm 1 dòng vào đây
+window.FN_TRACKS = [
+  {
+    id:    'track_1',
+    title: 'Leonell Cassio - Things Will Never Be the Same (ft. Carrie)',
+    src:   'music/leonellcassio-leonell-cassio-things-will-never-be-the-same-ft-carrie-270238.mp3'
+  },
+  {
+    id:    'track_2',
+    title: 'Leonell Cassio - The Paranormal Is Real (ft. Carrie)',
+    src:   'music/leonellcassio-leonell-cassio-the-paranormal-is-real-ft-carrie-163742.mp3'
+  },
+  {
+    id:    'track_3',
+    title: 'Leonell Cassio - A Trip to the Trees (ft. Carrie)',
+    src:   'music/leonellcassio-leonell-cassio-a-trip-to-the-trees-ft-carrie-171304.mp3'
+  },
+  {
+    id:    'track_4',
+    title: 'Leonell Cassio - The Paranormal Is Real (ft. Carrie)',
+    src:   'music/leonellcassio-leonell-cassio-the-paranormal-is-real-ft-carrie-163742.mp3'
+  },
+  {
+    id:    'track_5',
+    title: 'Leonell Cassio - Things Will Never Be the Same (ft. Carrie)',
+    src:   'music/leonellcassio-leonell-cassio-things-will-never-be-the-same-ft-carrie-270238.mp3'
+  },
+];
