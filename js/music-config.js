d6053a30b5b53729d84009059578fb82ef1bce93
haznox2260 <tinhha226@gmail.com>
2026-07-02 18:34:15 +0700

    Update music-config.js

--- js/music-config.js
+++ js/music-config.js
@@ -16,126 +16,111 @@ window.FN_TRACKS = [
   },
   {
     id:    'track_4',
-    title: 'Leonell Cassio - It\'s Okay to Cry (ft. Carrie)',
-    src:   'music/leonellcassio-leonell-cassio-itx27s-okay-to-cry-ft-carrie-127087.mp3'
-  },
-  {
-    id:    'track_5',
     title: 'Leonell Cassio - Same Red (ft. Anne Lan)',
     src:   'music/leonellcassio-leonell-cassio-same-red-ft-anne-lan-119071.mp3'
   },
   {
-    id:    'track_6',
+    id:    'track_5',
     title: 'Leonell Cassio - Memories (ft. Anne Lan)',
     src:   'music/leonellcassio-leonell-cassio-memories-ft-anne-lan-119069.mp3'
   },
   {
-    id:    'track_7',
+    id:    'track_6',
     title: 'Leonell Cassio - The Blackest Bouquet (ft. Carrie)',
     src:   'music/leonellcassio-leonell-cassio-the-blackest-bouquet-118766.mp3'
   },
   {
-    id:    'track_8',
+    id:    'track_7',
     title: 'Leonell Cassio - Rocks n Mud (ft. Krista Marina)',
     src:   'music/leonellcassio-leonell-cassio-rocks-n-mud-ft-krista-marina-118765.mp3'
   },
   {
-    id:    'track_9',
+    id:    'track_8',
     title: 'Leonell Cassio - Say It to My Face (ft. Dylan Emmet)',
     src:   'music/leonellcassio-leonell-cassio-say-it-to-my-face-ft-dylan-emmet-118763.mp3'
   },
   {
-    id:    'track_10',
+    id:    'track_9',
     title: 'Leonell Cassio - Out of Time (ft. Lily Hain)',
     src:   'music/leonellcassio-leonell-cassio-out-of-time-ft-lily-hain-118760.mp3'
   },
   {
-    id:    'track_11',
+    id:    'track_10',
     title: 'Alex Morgan - Phonk Brazilian Phonk Phonk Music',
     src:   'music/alex-morgan-phonk-brazilian-phonk-phonk-music-545509.mp3'
   },
   {
-    id:    'track_12',
+    id:    'track_11',
     title: 'Leonell Cassio - The Sapphire City',
     src:   'music/leonellcassio-leonell-cassio-the-sapphire-city-10450.mp3'
   },
   {
-    id:    'track_13',
+    id:    'track_12',
     title: 'Leonell Cassio - Stuck in a Dream (ft. Juhi Perumal)',
     src:   'music/leonellcassio-leonell-cassio-stuck-in-a-dream-ft-juhi-perumal-110345.mp3'
   },
   {
-    id:    'track_14',
+    id:    'track_13',
     title: 'Leonell Cassio - Chapter Two (ft. Carrie)',
     src:   'music/leonellcassio-leonell-cassio-chapter-two-ft-carrie-114909.mp3'
   },
   {
-    id:    'track_15',
+    id:    'track_14',
     title: 'Leonell Cassio - Sick & Tired (ft. Lily Hain)',
     src:   'music/leonellcassio-leonell-cassio-sick-amp-tired-ft-lily-hain-118647.mp3'
   },
   {
-    id:    'track_16',
-    title: 'Leonell Cassio - Love Your Moon (ft. Gerhard)',
-    src:   'music/leonellcassio-leonell-cassio-love-your-moon-ft-gerhard-118648.mp3'
-  },
-  {
-    id:    'track_17',
+    id:    'track_15',
     title: 'Leonell Cassio - Chasin\' Balloons (ft. Krista Marina)',
     src:   'music/leonellcassio-leonell-cassio-chasinx27-balloons-ft-krista-marina-118649.mp3'
   },
   {
-    id:    'track_18',
+    id:    'track_16',
     title: 'Leonell Cassio - A Magical Journey Through Space',
     src:   'music/leonellcassio-leonell-cassio-a-magical-journey-through-space-118650.mp3'
   },
   {
-    id:    'track_19',
+    id:    'track_17',
     title: 'Leonell Cassio - Lie 2 You (ft. Dylan Emmet)',
     src:   'music/leonellcassio-leonell-cassio-lie-2-you-ft-dylan-emmet-118754.mp3'
   },
   {
-    id:    'track_20',
-    title: 'Leonell Cassio - Me 4 Me (ft. Julia Mihevc)',
-    src:   'music/leonellcassio-leonell-cassio-me-4-me-ft-julia-mihevc-118758.mp3'
-  },
-  {
-    id:    'track_21',
+    id:    'track_18',
     title: 'Leonell Cassio - Night Sky (ft. Julia Mihevc)',
     src:   'music/leonellcassio-leonell-cassio-night-sky-ft-julia-mihevc-118759.mp3'
   },
   {
-    id:    'track_22',
+    id:    'track_19',
     title: 'Mondamusic - Phonk Aura',
     src:   'music/mondamusic-phonk-phonk-aura-542549.mp3'
   },
   {
-    id:    'track_23',
+    id:    'track_20',
     title: 'Alex Morgan - Tokyo Night Walk',
     src:   'music/alex-morgan-tokyo-night-walk-537446.mp3'
   },
   {
-    id:    'track_24',
+    id:    'track_21',
     title: 'Alex Morgan - Indie Rock Dreamy',
     src:   'music/alex-morgan-indie-rock-dreamy-537459.mp3'
   },
   {
-    id:    'track_25',
+    id:    'track_22',
     title: 'Alex Morgan - Corporate',
     src:   'music/alex-morgan-corporate-530945.mp3'
   },
   {
-    id:    'track_26',
+    id:    'track_23',
     title: 'Alex Morgan - Future Bass',
     src:   'music/alex-morgan-future-bass-545515.mp3'
   },
   {
-    id:    'track_27',
+    id:    'track_24',
     title: 'Alex Morgan - Corporate Background Music',
     src:   'music/alex-morgan-corporate-background-music-548621.mp3'
   },
   {
-    id:    'track_28',
+    id:    'track_25',
     title: 'Apalon Beats - House Background Music',
     src:   'music/apalonbeats-house-background-music-549535.mp3'
   }
